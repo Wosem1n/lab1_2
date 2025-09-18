505685e6375502f7276d4174030578f080fd1f65
Wosem1n <makar.egorof@gmail.com>
2025-09-18 15:17:31 +0500

    Сохраняю изменения

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ for i in range(n):
 
 print("Изначальный массив: ", numbers)
 
-for i in range(n):
-    for j in range(0, n - i - 1):
-        if numbers[j] > numbers[j + 1]:
-            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
-print("Отсортированный массив: ", numbers)
+question = input("В каком направлении осуществлять сортировку? (v-по возрастанию, u-по убыванию): ")
+if question == "v":
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    print("Отсортированный массив: ", numbers)
+elif question == "u":
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if numbers[j] < numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    print("Отсортированный массив: ", numbers)
+else:
+    print("Вы ввели что-то не то!")
+
 
